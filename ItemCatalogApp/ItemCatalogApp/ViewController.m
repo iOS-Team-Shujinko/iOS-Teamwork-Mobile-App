85bf85c0d1e38d0ed214d8277b3fbc68cb5cc3ee
Georgi Yanakiev <yanakiev_g@outlook.com>
2014-11-04 00:09:56 +0200

    Initial Commit

---
+++ ItemCatalogApp/ItemCatalogApp/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  ItemCatalogApp
+//
+//  Created by m3tr0n0m3 on 11/4/14.
+//  Copyright (c) 2014 Shujinko. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
---
+++ ItemCatalogApp/ItemCatalogApp/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  ItemCatalogApp
+//
+//  Created by m3tr0n0m3 on 11/4/14.
+//  Copyright (c) 2014 Shujinko. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
